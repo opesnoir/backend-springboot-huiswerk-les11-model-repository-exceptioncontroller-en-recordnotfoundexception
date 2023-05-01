813be8479edd6fa9e75dbec1017f15d9b8132aa6
opesnoir <goodsurroundsme@outlook.com>
2023-05-01 18:43:15 +0200

    exception controller toegevoegd, volgens de edhub syntax

---
+++ src/main/java/controller/ExceptionController.java
@@ -0,0 +1,20 @@
+package controller;
+
+// Spring Boot heeft een annotatie @ControllerAdvice waarmee een speciale Exception-Controller kan
+//worden aangegeven. Een exception eindigt dan in een controller-methode die als response de juiste status code teruggeeft.
+
+import exception.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<Object> exception(RecordNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
+
+
