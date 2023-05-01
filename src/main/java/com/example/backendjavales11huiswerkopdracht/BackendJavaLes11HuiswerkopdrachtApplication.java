8aac88461c50bd12d8ed246a78489bb7f7b08266
opesnoir <goodsurroundsme@outlook.com>
2023-05-01 13:13:13 +0200

    mappen gemaakt, propertiestoegevoegd, database gemaakt en aan properties toegevoegd

---
+++ src/main/java/com/example/backendjavales11huiswerkopdracht/BackendJavaLes11HuiswerkopdrachtApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendjavales11huiswerkopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaLes11HuiswerkopdrachtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendJavaLes11HuiswerkopdrachtApplication.class, args);
+    }
+
+}
