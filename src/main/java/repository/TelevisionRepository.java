75d010255629af374de82ffd348fa8133c3d03bb
opesnoir <goodsurroundsme@outlook.com>
2023-05-01 18:26:24 +0200

    exception record not found aangemaakt, let op elke exception krijgt zijn eigen klasse

--- src/main/java/repository/TelevisionRepository.java
+++ src/main/java/repository/TelevisionRepository.java
@@ -1,7 +1,11 @@
 package repository;
 
 import model.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TelevisionRepository extends CrudRepository<Television, Long> {
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
+
+
+
 }
