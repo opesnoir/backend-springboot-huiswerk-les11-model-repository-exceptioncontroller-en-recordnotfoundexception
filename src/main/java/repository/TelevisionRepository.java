7bd1761d73bd980671e7c89eadb05a13fe0b7093
opesnoir <goodsurroundsme@outlook.com>
2023-05-01 13:25:19 +0200

    Televison klas voorzien van variabelen, constructor en getters en setters

---
+++ src/main/java/repository/TelevisionRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import model.Television;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TelevisionRepository extends CrudRepository<Television, Long> {
+}
