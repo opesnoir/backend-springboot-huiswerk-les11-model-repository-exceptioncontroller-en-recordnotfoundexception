fef8c4368899f688143119ea45d196a891be310e
opesnoir <goodsurroundsme@outlook.com>
2023-05-02 18:58:32 +0200

    getmapping voor de lijst tv's en voor een enkele tv toegevoegd, beide inclusief if-statement. De ene retouneert een record not found exception en  de andere een methode notfound()

--- src/main/java/com/example/backendjavales11huiswerkopdracht/controller/TelevisionController.java
+++ src/main/java/com/example/backendjavales11huiswerkopdracht/controller/TelevisionController.java
@@ -1,15 +1,48 @@
 package com.example.backendjavales11huiswerkopdracht.controller;
 
+import com.example.backendjavales11huiswerkopdracht.exception.RecordNotFoundException;
+import com.example.backendjavales11huiswerkopdracht.model.Television;
+import com.example.backendjavales11huiswerkopdracht.repository.TelevisionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController //
+import java.util.List;
+
+@RestController //deze class is een rest controller
 @RequestMapping("/televisions") //root path
 public class TelevisionController {
-/*
-    //lijst met alle tv's
-    @GetMapping
+    @Autowired//springboot maakt repository automatisch
+    //variabel
+    private TelevisionRepository repository;
 
-    //lijst met een enkele tv, op id
+    //lijst met alle tv's, inclusief if-statement met record not found exception melding voor als er geen televisies worden gevonden.
+    @GetMapping
+    public ResponseEntity<List<Television>> getTelevisions() {
+        List<Television> televisions = repository.findAll();
+        if (televisions.isEmpty()) {
+            throw new RecordNotFoundException("No televisions found.");
+        } else {
+            return ResponseEntity.ok(televisions);
+        }
+    }
+
+    //lijst met een enkele tv, inclusief if-statement met record not found exception melding voor als de tv niet wordt gevonden
+    @GetMapping("/{id}")
+    public ResponseEntity<Television> getTelevision(@PathVariable int id) {
+        List<Television> televisions = repository.findAll();
+        if (id >= 0 && id < televisions.size()) {
+            Television television = televisions.get(id);
+            return ResponseEntity.ok(television);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+/*    //lijst met een enkele tv, op id
     @GetMapping("/{id}")
 
     //tv toevoegen
