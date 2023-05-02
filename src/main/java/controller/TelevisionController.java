e9de0fcc56a6f7f71cce21d7ad14bc8cb0b2fd9e
opesnoir <goodsurroundsme@outlook.com>
2023-05-02 11:09:22 +0200

    televisioncontroller opgezet, met get, post, put en deletemapping

--- src/main/java/controller/TelevisionController.java
+++ src/main/java/controller/TelevisionController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController //
+@RequestMapping("/televisions") //root path
 public class TelevisionController {
+
+    //lijst met alle tv's
+    @GetMapping
+
+    //lijst met een enkele tv, op id
+    @GetMapping("/{id}")
+
+    //tv toevoegen
+    @PostMapping("/{id}")
+
+    //tv aanpassen
+    @PutMapping("/{id}")
+
+    //tv verwijderen
+    @DeleteMapping("/{id}")
+
+
 }
